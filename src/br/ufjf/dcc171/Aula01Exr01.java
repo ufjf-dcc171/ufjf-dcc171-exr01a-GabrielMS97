55fb60ad22ed8af1f2b451a126113f74ef4a16a5
Gabriel Martins <gabrielmartinssantana@hotmail.com>
2017-08-09 14:08:28 -0300

    Exercício concluído

--- src/br/ufjf/dcc171/Aula01Exr01.java
+++ src/br/ufjf/dcc171/Aula01Exr01.java
@@ -1,21 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.ufjf.dcc171;
 
-/**
- *
- * @author ice
- */
+import java.util.InputMismatchException;
+import javax.swing.JOptionPane;
+
 public class Aula01Exr01 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            float c, l, a;
+            String s1, s2, s3;
+            s1 = JOptionPane.showInputDialog("Digite o comprimento do pacote: ");
+            if (s1 == null) {
+                JOptionPane.showMessageDialog(null, "Você deveria ter digitado um número!", "ERRO!", JOptionPane.ERROR_MESSAGE);
+            }
+            s2 = JOptionPane.showInputDialog("Digite a largura do pacote: ");
+            if (s2 == null) {
+                JOptionPane.showMessageDialog(null, "Você deveria ter digitado um número!", "ERRO!", JOptionPane.ERROR_MESSAGE);
+            }
+            s3 = JOptionPane.showInputDialog("Digite a altura do pacote: ");
+            if (s3 == null) {
+                JOptionPane.showMessageDialog(null, "Você deveria ter digitado um número!", "ERRO!", JOptionPane.ERROR_MESSAGE);
+            }
+
+            c = Float.parseFloat(s1);
+            l = Float.parseFloat(s2);
+            a = Float.parseFloat(s3);
+
+            JOptionPane.showMessageDialog(null, "O volume do pacote é " + (c * l * a) + "cm³", "Resultado", JOptionPane.INFORMATION_MESSAGE);
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(null, "Informe apenas valores numéricos!", "ERRO!", JOptionPane.ERROR_MESSAGE);
+
+        }
     }
-    
+
 }
